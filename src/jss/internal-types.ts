067463cead8ac4ded9a6201047b8081c7472f8c8
Steve Springett <steve@springett.us>
2026-04-27 16:45:46 -0500

    add Apache-2.0 license header to all source files
    Signed-off-by: Steve Springett <steve@springett.us>

--- src/jss/internal-types.ts
+++ src/jss/internal-types.ts
@@ -1,3 +1,22 @@
+/*!
+This file is part of CycloneDX Signing Library for Javascript.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+SPDX-License-Identifier: Apache-2.0
+Copyright (c) OWASP Foundation. All Rights Reserved.
+*/
+
 /**
  * JSS-internal types used by the binding and the sign/verify
  * orchestration. Not exported through the public API; consumers see
